4632e1f130479a396bf3a590726f2fde23c2f99b
saud786ahmad <111411978+saud786ahmad@users.noreply.github.com>
2022-09-17 00:21:41 +0530

    Update AuthenticationServiceApplicationTests.java

--- authentication-service/src/test/java/com/stackroute/AuthenticationServiceApplicationTests.java
+++ authentication-service/src/test/java/com/stackroute/AuthenticationServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.stackroute;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AuthenticationServiceApplicationTests.class)
 class AuthenticationServiceApplicationTests {
 
 	@Test
